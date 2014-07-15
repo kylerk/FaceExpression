8b453293eb66d22f82212e6cbe16c528c2ed2e5d
kylerk <kylerk@gmail.com>
2014-07-15 18:37:11 -0400

    The whole controller input thing has been completed.  There might be ways to make it more compressed but I'm not sure how.

--- FaceUnity/Assets/Scripts/ControllerInput.cs
+++ FaceUnity/Assets/Scripts/ControllerInput.cs
@@ -18,7 +18,15 @@ public class ControllerInput : MonoBehaviour
 		GameObject StartBut;
 		GameObject Back;
 
-		
+
+		Vector3 Aposition;
+		Vector3 Bposition;
+		Vector3 Xposition;
+		Vector3 Yposition;
+		Vector3 StartButPosition;
+		Vector3 BackPosition;
+		Vector3 LStickPosition;
+		Vector3 RStickPosition;
 
 		// Use this for initialization
 		void Start ()
@@ -35,8 +43,17 @@ public class ControllerInput : MonoBehaviour
 				LShoulder = GameObject.Find ("LShoulder");
 				RShoulder = GameObject.Find ("RShoulder");
 				StartBut = GameObject.Find ("Start");
-				Back = GameObject.Find ("Back");
-	
+				Back = GameObject.Find ("Select");
+
+		 
+				Aposition = A.transform.position;
+				Bposition = B.transform.position;
+				Xposition = X.transform.position;
+				Yposition = Y.transform.position;
+				StartButPosition = StartBut.transform.position;
+				BackPosition = Back.transform.position;
+				LStickPosition = LStick.transform.position;
+				RStickPosition = RStick.transform.position;
 		}
 
 		// Update is called once per frame
@@ -44,34 +61,69 @@ public class ControllerInput : MonoBehaviour
 		{
 			
 				ctrl.updateCtrl ();
+				StickTilt (LStick, 60, ctrl.LXStick, ctrl.LYStick);
+				StickTilt (RStick, 60, ctrl.RXStick, ctrl.RYStick);
+				ButtonPress (LStick, LStickPosition, new Vector3 (0, -3, 0), new Vector3 (1.4f, 0.5f, 1.4f), ctrl.LStickButton);
+				ButtonPress (RStick, RStickPosition, new Vector3 (0, -3, 0), new Vector3 (1.4f, 0.5f, 1.4f), ctrl.RStickButton);
 
-
-				LStick.transform.eulerAngles = new Vector3 (
-													Mathf.Lerp (0, 60, ctrl.LYStick) - 30,
-		                                            0,
-		                                            Mathf.Lerp (0, 60, ctrl.LXStick) - 30);
-				RStick.transform.eulerAngles = new Vector3 (
-													Mathf.Lerp (0, 60, ctrl.RYStick) - 30,
-													0,
-													Mathf.Lerp (0, 60, ctrl.RXStick) - 30);
+				StickTilt (DPad, 30, ctrl.DXPad, ctrl.DYPad);
 				
+				TriggerTilt (LTrigger, 60, ctrl.LTrigger);
+				TriggerTilt (RTrigger, 60, ctrl.RTrigger);
 
-				DPad.transform.eulerAngles = new Vector3 (
-													Mathf.Lerp (0, 10, ctrl.DYPad) - 5,
-													0,
-													Mathf.Lerp (0, 10, ctrl.DXPad) - 5);
-				
+				ShoulderTilt (LShoulder, 20, ctrl.LShoulder);
+				ShoulderTilt (RShoulder, -20, ctrl.RShoulder);
 				//A.transform.position = new Vector3 (ctrl.A, 0, 0);
 
-				A.transform.localPosition = new Vector3 (ctrl.A, 0, 0);
 				
-				Debug.Log (ctrl.A);
-	
+				ButtonPress (A, Aposition, new Vector3 (0, -3, 0), new Vector3 (1.4f, 0.5f, 1.4f), ctrl.A);
+				ButtonPress (B, Bposition, new Vector3 (0, -3, 0), new Vector3 (1.4f, 0.5f, 1.4f), ctrl.B);
+				ButtonPress (X, Xposition, new Vector3 (0, -3, 0), new Vector3 (1.4f, 0.5f, 1.4f), ctrl.X);
+				ButtonPress (Y, Yposition, new Vector3 (0, -3, 0), new Vector3 (1.4f, 0.5f, 1.4f), ctrl.Y);
+				ButtonPress (StartBut, StartButPosition, new Vector3 (0, -3, 0), new Vector3 (1.4f, 0.5f, 1.4f), ctrl.Start);
+				ButtonPress (Back, BackPosition, new Vector3 (0, -3, 0), new Vector3 (1.4f, 0.5f, 1.4f), ctrl.Back);
+
+				
+
 		}
+
+		void StickTilt (GameObject StickObj, float tiltRange, float inputX, float inputY)
+		{
+				StickObj.transform.eulerAngles = new Vector3 (
+			Mathf.Lerp (0, tiltRange, inputY) - tiltRange / 2,
+			0,
+			Mathf.Lerp (0, tiltRange, inputX) - tiltRange / 2);
+				
+		}
+
+		void ButtonPress (GameObject ButtonObj, Vector3 startposition, Vector3 movement, Vector3 squash, float input)
+		{
+				ButtonObj.transform.position = Vector3.Lerp (startposition, startposition + movement, input);	
+				ButtonObj.transform.localScale = Vector3.Lerp (new Vector3 (1, 1, 1), squash, input);
+		}
+
+		void TriggerTilt (GameObject TriggerObj, float tiltRange, float input)
+		{
+				TriggerObj.transform.eulerAngles = new Vector3 (
+
+			Mathf.Lerp (0, -tiltRange, input),
+			0,
+			0);
+		}
+
+		void ShoulderTilt (GameObject ShoulderObj, float tiltRange, float input)
+		{
+				ShoulderObj.transform.eulerAngles = new Vector3 (
+			0,
+			Mathf.Lerp (0, -tiltRange, input),
+
+			0);
+		}
+
 
 }
 
-public class Ctrl
+public class Ctrl  ////
 {
 
 		public float LYStick;
@@ -81,7 +133,17 @@ public class Ctrl
 		public float DYPad;
 		public float DXPad;
 		public float A;
-
+		public float B;
+		public float X;
+		public float Y;
+		public float Start;
+		public float Back;
+		public float LTrigger;
+		public float RTrigger;
+		public float LShoulder;
+		public float RShoulder;
+		public float LStickButton;
+		public float RStickButton;	
 
 		public void updateCtrl ()
 		{
@@ -94,6 +156,19 @@ public class Ctrl
 				DXPad = (Input.GetAxis ("DXPad") + 1) / 2;
 
 				A = (Input.GetAxis ("A"));
+				B = (Input.GetAxis ("B"));
+				X = (Input.GetAxis ("X"));
+				Y = (Input.GetAxis ("Y"));
+
+				Start = (Input.GetAxis ("Start"));
+				Back = (Input.GetAxis ("Back"));
+
+				LTrigger = (Input.GetAxis ("LTrigger"));
+				RTrigger = (Input.GetAxis ("RTrigger"));
+				LShoulder = (Input.GetAxis ("LShoulder"));
+				RShoulder = (Input.GetAxis ("RShoulder"));
+				LStickButton = (Input.GetAxis ("LStickPress"));
+				RStickButton = (Input.GetAxis ("RStickPress"));
 		}
 }
 
