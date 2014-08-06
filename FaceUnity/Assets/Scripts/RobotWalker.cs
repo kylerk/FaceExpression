5fb7c217384e548c50f99b5bcf352f243ccbec5e
kylerk <kylerk@gmail.com>
2014-08-06 18:07:04 -0400

    Made a Robot that moves along the surface of the controller.  Still really buggy movement, but it almost works.

---
+++ FaceUnity/Assets/Scripts/RobotWalker.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RobotWalker : MonoBehaviour
+{
+		bool newPosition = true;
+		RaycastHit hit;
+		float heading = 0f;
+		GameObject Robot;
+
+		// Use this for initialization
+		void Start ()
+		{
+				Physics.Raycast (transform.position, -transform.up, out hit, 10000f);
+				transform.position = hit.point;
+				transform.up = hit.normal;
+				Robot = GameObject.Find ("Robot");
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				
+				
+				transform.localPosition += Robot.transform.forward * -Ctrl.LYStick;
+
+				Debug.DrawRay (transform.position, transform.forward * 20, Color.blue, 0, false);
+				Debug.DrawRay (transform.position, transform.up * 20, Color.magenta, 0, false);
+
+				Physics.Raycast (transform.position + transform.up * 4, -transform.up, out hit, 10000f);
+				transform.position = hit.point;
+				
+				transform.up = hit.normal;
+				heading += Ctrl.LXStick * 3;
+				Robot.transform.Rotate (0, Ctrl.LXStick * 5, 0);
+
+		}
+
+
+
+		void groundToSurface ()
+		{
+
+
+		}
+}
---
+++ FaceUnity/Assets/Scripts/SurfaceWalker.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SurfaceWalker : MonoBehaviour
+{
+
+		public GameObject surface;
+		public Vector3 starttarget;
+		public Vector3 target;
+		RaycastHit hit;
+		Vector3 direction;
+		// Use this for initialization
+		void Start ()
+		{
+				direction = surface.transform.position - transform.position;
+				Physics.Raycast (transform.position, direction, out hit, 10000f);
+				transform.position = hit.point;
+				
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{		
+				Vector3 normal = new Vector3 (0, 1, 0);
+				RaycastHit hit;
+				
+				
+				Vector3 direction = target - starttarget;
+
+				//Debug.Log (direction);
+				Physics.Raycast (transform.position + normal * 30, direction, out hit, 10000f);
+				Debug.DrawLine (transform.position + normal * 30, direction, Color.cyan, 0, false);
+				normal = hit.normal;
+				transform.position = hit.point;
+				transform.Translate (-Ctrl.LXStick, 0, Ctrl.LYStick);
+				starttarget = transform.position + hit.normal * 10;
+				target = hit.point - hit.normal * 10;
+				
+
+				Debug.DrawLine (target, hit.point, Color.green, 0, false);
+				Debug.DrawRay (hit.point, hit.normal * 20, Color.blue, 0, false);
+
+				Debug.DrawRay (transform.position, transform.forward * 20, Color.red, 0, false);
+
+		}
+
+
+
+}
