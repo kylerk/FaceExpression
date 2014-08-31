e5630b2d43c143b8a5f5b5bb9f3247c9fb077cc2
kylerk <kylerk@gmail.com>
2014-08-30 22:07:58 -0400

    Made the Settle stuff work in 3 dimensions with vectors.  It actually made it super simple.  Added features that allow one trigger to act as a brake, and the other to act as a center point reset.

--- FaceUnity/Assets/Scripts/CameraSpin.cs
+++ FaceUnity/Assets/Scripts/CameraSpin.cs
@@ -7,33 +7,38 @@ public class CameraSpin : MonoBehaviour
 		public float stickForce;
 		public float settleGravity;
 		public float friction;
-		public AnimationCurve frictionCoefficient = new AnimationCurve (new Keyframe (0, 0), new Keyframe (1, 1));
-		float spinSpeedx;
-		float spinSpeedy;
-		float x = 0;
-		float y = 0;	
+		public float triggerBrake;
 		
-
+		public AnimationCurve frictionCoefficient = new AnimationCurve (new Keyframe (0, 0), new Keyframe (1, 1));
+		Vector3 xyz = new Vector3 ();  //currentPosition
+		Vector3 spinSpeedxyz = new Vector3 (); //Current Speed
+		Vector3 settleTarget = new Vector3 ();  //Where the vector will be drawn to
+		bool settleTargetPressed = false;
 	
 		// Update is called once per frame
 		void Update ()
 		{
-				//Attribute a certain point, spinSpeedx will reach an equilibrium where stick force no longer adds any more
-				spinSpeedx += -Ctrl.RXStick * stickForce;
-				spinSpeedx -= settleGravity * x;
+				//Here we can press the Ltrigger past half to set the settleTarget to the current position
+				if (Ctrl.LTrigger > 0.5 && !settleTargetPressed) {
+						settleTarget = xyz;
+						settleTargetPressed = true;
+				} else if (Ctrl.LTrigger < 0.5) {
+						settleTarget = new Vector3 (0, 0, 0);
+						settleTargetPressed = false;
+				}
 				
-				spinSpeedy += Ctrl.RYStick * stickForce;
-				spinSpeedy -= settleGravity * y;
-
-			
-				x += spinSpeedx;
-				y += spinSpeedy;
-				spinSpeedx *= friction * frictionCoefficient.Evaluate (Mathf.Abs (x / (stickForce / settleGravity)));
-				spinSpeedy *= friction * frictionCoefficient.Evaluate (Mathf.Abs (y / (stickForce / settleGravity)));
-				//	Debug.Log ("rangemap:" + Mathf.Abs (x / (stickForce / settleGravity)));
-				//		Debug.Log ("frictionEva:" + Mathf.Abs (x * stickForce / settleGravity));
-				//Debug.Log ("y:" + x);
-				transform.eulerAngles = new Vector3 (y % 360, x % 360, 0);
-
+				//Here we determine how much speed is being added to the current speed, both based on the inputs
+				spinSpeedxyz += new Vector3 (-Ctrl.RXStick, Ctrl.RYStick, Ctrl.LYStick) * stickForce 
+				//and how much acceleration is due to the settle Gravity		
+						+ (settleTarget - xyz) * settleGravity;
+				
+				
+				float currentFriction = 
+				Mathf.Max (0.0f, (friction - (Ctrl.RTrigger * triggerBrake)))
+						* frictionCoefficient.Evaluate (Mathf.Abs ((settleTarget - xyz).magnitude / (stickForce / settleGravity)));
+				spinSpeedxyz *= currentFriction;
+				xyz += spinSpeedxyz;
+	
+				transform.eulerAngles = new Vector3 (xyz [1] % 360, xyz [0] % 360, xyz [2] % 360);
 		}
 }
