3a334d6063b3f3a20fdf4a9307679ddb289f6b6d
kylerk <kylerk@gmail.com>
2014-09-29 12:36:08 -0400

    - Completely revamped code for spin - New Sprite sheets with more frames. - Paloma's example file is implemented.

---
+++ SpinExperiments/Assets/Scripts/CursorLockClick.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorLockClick : MonoBehaviour
+{	
+	void OnGUI ()
+	{
+		//GUILayout.Button ("Press Me!");
+		//Debug.Log ("id: " + GUIUtility.hotControl);
+	}
+	// Use this for initialization
+	void Start ()
+	{
+		//Screen.lockCursor = fals;
+		//guiTexture.enabled = true;
+		
+	}
+
+
+	void OnMouseDown ()
+	{
+		Screen.lockCursor = true;
+		guiTexture.enabled = false;
+	}
+	
+
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Screen.lockCursor = false;
+			guiTexture.enabled = true;
+			Debug.Log ("switchLock");
+		}
+
+
+	
+	}
+
+
+
+
+}
--- SpinExperiments/Assets/Scripts/SpinAround.cs
+++ SpinExperiments/Assets/Scripts/SpinAround.cs
@@ -1,45 +1,83 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
+
 public class SpinAround : MonoBehaviour
 {
-
+		public Texture sheet;
 		Sprite[] sprites;
 		SpriteRenderer spriteRenderer;
 		// Use this for initialization
 
 		public GameObject player;
+		//public string sheetname;
+		
+		public float angle;
 
+		public float hAngle;
+		public float vAngle;
+		public int inframe;
+		public int outframe;
+
+
+		public enum spinDir
+		{
+				forward,
+				reverse }
+		;
+		public spinDir spinDirection;
 
 		void Start ()
 		{
-				sprites = Resources.LoadAll<Sprite> ("GuySpin_V_1_w162_H285");
+				sprites = Resources.LoadAll<Sprite> (sheet.name);
 				spriteRenderer = GetComponent<SpriteRenderer> ();
 				spriteRenderer.sprite = sprites [0];
+
 		}
 
 
-		void FixedUpdate ()
-		{
-		
-				//transform.forward = transform.position - player.transform.position;
-			
-				Quaternion lookDirection = Quaternion.LookRotation (transform.position - player.transform.position, Vector3.up) * Quaternion.Euler (0, 1, 0);
-			
-				transform.rotation = Quaternion.Euler (new Vector3 (0, lookDirection.eulerAngles.y, 0));
-
-
-				int frame = Mathf.FloorToInt ((transform.eulerAngles.y - transform.parent.eulerAngles.y) / 360 * sprites.Length);
-				
-				spriteRenderer.sprite = sprites [frame];
-				//Debug.Log (frame);
-				//Debug.Log (transform.eulerAngles.y);
-		}
-		// Update is called once per frame
 		void Update ()
 		{
+
+				Vector3 _position = transform.position;
+				Vector3 _parentForward = transform.parent.forward;
+				Vector3 towardsPlayer = player.transform.position - transform.position;
+
+				Debug.DrawRay (_position, towardsPlayer, Color.red);
+				Debug.DrawRay (_position, _parentForward, Color.green);
+
+
+				Quaternion hlookDirection = Quaternion.LookRotation (towardsPlayer, Vector3.up);
+			
+				
+
+				angle = hlookDirection.eulerAngles.y;
 	
+				transform.rotation = Quaternion.Euler (new Vector3 (0, angle, 0));
+				
+				hAngle = transform.localEulerAngles.y;
+	
+
+
+				vAngle = Vector3.Angle (transform.forward, towardsPlayer);
+
+
+				Debug.DrawRay (transform.position, transform.forward);
+
+				inframe = Mathf.Clamp (Mathf.FloorToInt (hAngle / 360 * sprites.Length), 0, sprites.Length - 1);
+				
+
+				if (spinDirection == spinDir.forward) {
+						outframe = sprites.Length - 1 - inframe;
+				} else if (spinDirection == spinDir.reverse) {
+						outframe = inframe;
+				}
+		
+				spriteRenderer.sprite = sprites [outframe];
 		}
+
+
 
 
 }
